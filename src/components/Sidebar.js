2ad95cd194ae5715f66a0f57eae5c3819ed7e934
Wenzel <wenzelbarboza@gmail.com>
2022-08-08 21:27:34 +0530

    working on sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,6 +2,15 @@ import React from 'react'
 import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 import CreateIcon from '@mui/icons-material/Create';
 import MessageIcon from '@mui/icons-material/Message';
+import InboxIcon from '@mui/icons-material/Inbox';
+import DraftsIcon from '@mui/icons-material/Drafts';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import AppsIcon from '@mui/icons-material/Apps';
+import FileCopyIcon from '@mui/icons-material/FileCopy';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AddIcon from '@mui/icons-material/Add';
 import './Sidebar.css'
 import SidebarOption from './SidebarOption';
 
@@ -18,8 +27,19 @@ const Sidebar = () => {
                 </div>
                 <CreateIcon />
             </div>
-            <SidebarOption Icon={MessageIcon} text='chat name' />
-            <SidebarOption text='chat name' />
+            <SidebarOption Icon={MessageIcon} text='Threads' />
+            <SidebarOption Icon={InboxIcon} text='Mentions & reactions' />
+            <SidebarOption Icon={DraftsIcon} text='Saved items' />
+            <SidebarOption Icon={BookmarkBorderIcon} text='Channel browser' />
+            <SidebarOption Icon={PeopleAltIcon} text='People and user groups' />
+            <SidebarOption Icon={AppsIcon} text='Apps' />
+            <SidebarOption Icon={FileCopyIcon} text='File browser' />
+            <SidebarOption Icon={ExpandLessIcon} text='show less' />
+            <hr />
+            <SidebarOption Icon={ExpandMoreIcon} text='chat name' />
+            <hr />
+            <SidebarOption Icon={AddIcon} text='chat name' />
+
         </div>
     )
 }
